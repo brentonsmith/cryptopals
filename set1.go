b7c3d0c06095c7bfd584ee955db636d0978d9106
brentonsmith <brenton.smith94@gmail.com>
2016-09-18 21:49:27 +1000

    challenge 2 done

--- set1.go
+++ set1.go
@@ -3,9 +3,30 @@ package cryptopals
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 )
 
 func Challenge1(h string) string {
 	hexStringAsBytes, _ := hex.DecodeString(h)
 	return base64.StdEncoding.EncodeToString(hexStringAsBytes)
+}
+
+func Challenge2(i1, i2 string) (string, error) {
+	if len(i1) != len(i2) {
+		return "", errors.New("Length mismatch!")
+	}
+
+	length := hex.DecodedLen(len(i1))
+	bytes1 := make([]byte, length)
+	bytes2 := make([]byte, length)
+	output := make([]byte, length)
+
+	hex.Decode(bytes1, []byte(i1))
+	hex.Decode(bytes2, []byte(i2))
+
+	for i := 0; i < len(bytes1); i++ {
+		output[i] = bytes1[i] ^ bytes2[i]
+	}
+
+	return hex.EncodeToString(output), nil
 }
--- set1_test.go
+++ set1_test.go
@@ -8,7 +8,40 @@ func TestChallenge1(t *testing.T) {
 	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 	actual := Challenge1(hexToEncode)
 
-	if actual != expected {
+	if expected != actual {
 		t.Errorf("Expected: %s \n Actual: %s", expected, actual)
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	in1 := "1c0111001f010100061a024b53535009181c"
+	in2 := "686974207468652062756c6c277320657965"
+
+	expected := "746865206b696420646f6e277420706c6179"
+	actual, err := Challenge2(in1, in2)
+
+	if err != nil {
+		t.Error("Expected no error!")
+	}
+
+	if expected != actual {
+		t.Errorf("Expected: %s \n Actual: %s", expected, actual)
+	}
+}
+
+func TestChallenge2_HandlesUnevenInputs(t *testing.T) {
+	in1 := "1c0"
+	in2 := "68697"
+
+	_, err1 := Challenge2(in1, in2)
+
+	if err1 == nil {
+		t.Error("Expected an error!")
+	}
+
+	_, err2 := Challenge2(in2, in1)
+
+	if err2 == nil {
+		t.Error("Expected an error!")
 	}
 }
